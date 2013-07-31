d1a27c3a2d8fae2e81ead2dd27431e29ef72416a
Roman Diachuk <roman.dyachuk@gmail.com>
2013-08-01 00:21:56 +0300

    Initial commit

---
+++ alka-website/src/main/java/com/family/diachuk/alkawebsite/beans/HelloBean.java
@@ -0,0 +1,33 @@
+package com.family.diachuk.alkawebsite.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
